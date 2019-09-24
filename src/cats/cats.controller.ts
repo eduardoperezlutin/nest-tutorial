79cb9ea2259fe6c54841f409cc8133f621ab392a
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-09-23 23:37:37 -0600

    Post for cats controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,6 +3,11 @@ import { create } from 'istanbul-reports';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'This adds a new cat';
+  }
+
   @Get()
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
