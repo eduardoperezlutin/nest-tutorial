9ba682abedb8694e71deb131f0b5ee9efcd80a2a
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-09-24 00:01:03 -0600

    GET Single passing params

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Req, Post, HttpCode } from '@nestjs/common';
-import { create } from 'istanbul-reports';
+import { Controller, Get, Post, HttpCode, Header, Param } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Post()
+  @Header('Cache-Control', 'none')
   @HttpCode(204)
   create() {
     return 'This adds a new cat';
@@ -12,5 +12,11 @@ export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
